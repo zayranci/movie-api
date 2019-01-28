2d3e1b84a55190039d8f22129a90d9cbd3c6b82a
zayranci <zayranci.za@gmail.com>
2019-01-28 15:13:29 +0300

    /api/directors/:director_id [PUT]

--- routes/director.js
+++ routes/director.js
@@ -1,3 +1,4 @@
+const mongoose = require ('mongoose');
 const express = require('express');
 const router = express.Router();
 
@@ -60,7 +61,79 @@ router.get('/',(req,res)=>{
   }).catch((err)=>{
     res.json(err);
   });
-  
+});
+
+
+router.get('/:director_id',(req,res)=>{
+  //belirli bir yönetmene ulaşmak için endpoint işlemi
+  const promise = Director.aggregate([
+    {
+      $match:{
+        '_id': mongoose.Types.ObjectId(req.params.director_id)
+      }
+    },
+    {
+      $lookup:{
+        from:'movies',
+        localField: '_id',
+        foreignField: 'director_id',
+        as: 'movies'
+      }
+    },
+    {
+      $unwind:{
+        path: '$movies',
+        preserveNullAndEmptyArrays: true
+      }
+    },
+    {
+      $group: {
+        _id: {
+          _id: '$_id',
+          name: '$name',
+          surname: '$surname',
+          bio: '$bio'
+        },
+        movie: {
+          $push: '$movies'
+        }
+      }
+    },
+    {
+      $project: {
+        _id: '$_id._id',
+        name: '$_id.name',
+        surname: '$_id.surname',
+        movies: '$movie'
+      }
+    }
+  ]);
+  promise.then((data)=>{
+    res.json(data);
+  }).catch((err)=>{
+    res.json(err);
+  });
+
+});
+
+//yönetmen güncelleme endpoint
+router.put('/:director_id',(req,res,next)=>{
+  const promise = Director.findByIdAndUpdate(
+      req.params.director_id,
+      req.body,
+      {
+        new:true
+      }
+  );
+
+  promise.then((director)=>{
+    if(!director)
+      next({message: 'The director was not found.', code:999});
+
+    res.json(director);
+  }).catch((err)=>{
+    res.json(err);
+  });
 });
 
 module.exports = router;
