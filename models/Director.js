e88595b31f1987dc770c656b4c47f5a24a5741e4
zayranci <zayranci.za@gmail.com>
2019-01-28 16:26:44 +0300

    models/Director validation schema for movie

--- models/Director.js
+++ models/Director.js
@@ -2,9 +2,21 @@ const mongoose = require('mongoose');
 const Schema= mongoose.Schema;
 
 const DirectorSchema = new Schema({
-    name: String,
-    surname: String,
-    bio: String,
+    name: {
+        type: String,
+        maxlength: [60, '`{PATH}` alanı ({VALUE}),({MAXLENGTH}) karakteden az olmalıdır.'],
+        minlength: [2, '`{PATH}` alanı ({VALUE}),({MINLENGTH}) karakteden çok olmalıdır.']
+    },
+    surname:{
+        type: String,
+        maxlength: [60, '`{PATH}` alanı ({VALUE}),({MAXLENGTH}) karakteden az olmalıdır.'],
+        minlength: [2, '`{PATH}` alanı ({VALUE}),({MINLENGTH}) karakteden çok olmalıdır.']
+    },
+    bio:{
+        type: String,
+        maxlength: [1000, '`{PATH}` alanı ({VALUE}),({MAXLENGTH}) karakteden az olmalıdır.'],
+        minlength: [2, '`{PATH}` alanı ({VALUE}),({MINLENGTH}) karakteden çok olmalıdır.']
+    },
     createdAt:{
         type:Date,
         default:Date.now
