43226c8cd1121bb32691e6983738cd46cdbd583d
zayranci <zayranci.za@gmail.com>
2019-01-28 14:53:11 +0300

    api/directors endpoint [GET]

--- routes/director.js
+++ routes/director.js
@@ -11,8 +11,56 @@ router.post('/', (req, res, next)=>{
   promise.then((data)=>{
     res.json(data);
   }).catch((err)=>{
-    res.json(data);
+    res.json(err);
   })
+});
+
+router.get('/',(req,res)=>{
+
+  //join işlemi için
+  const promise = Director.aggregate([
+    {
+      $lookup:{
+        from:'movies',
+        localField: '_id',
+        foreignField: 'director_id',
+        as: 'movies'
+      }
+    },
+    {
+      $unwind:{
+        path: '$movies',
+        preserveNullAndEmptyArrays: true
+      }
+    },
+    {
+      $group: {
+        _id: {
+          _id: '$_id',
+          name: '$name',
+          surname: '$surname',
+          bio: '$bio'
+        },
+        movie: {
+          $push: '$movies'
+        }
+      }
+    },
+    {
+      $project: {
+        _id: '$_id._id',
+        name: '$_id.name',
+        surname: '$_id.surname',
+        movies: '$movie'
+      }
+    }
+  ]);
+  promise.then((data)=>{
+    res.json(data);
+  }).catch((err)=>{
+    res.json(err);
+  });
+  
 });
 
 module.exports = router;
