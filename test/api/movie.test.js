040dee4ebf3039bf7a138ae5853f7b85b24ac931
zayranci <zayranci.za@gmail.com>
2019-01-29 16:37:00 +0300

    api/movies/[POST] test

--- test/api/movie.test.js
+++ test/api/movie.test.js
@@ -31,4 +31,32 @@ describe('/api/movies tests', ()=>{
         })
     });
 
+    describe('/POST Movie',()=>{
+        it('it should POST a movie', (done)=>{
+            const movie = {
+                title:'Udemy',
+                director_id:'5c4da5802b8c211638a23045',
+                category: 'comedy',
+                country:'Turkey',
+                year:1950,
+                imdb_score:8
+            };
+            chai.request(server)
+                .post('/api/movies')
+                .send(movie)
+                .set('x-access-token',token)
+                .end((err,res)=>{
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('title');
+                    res.body.should.have.property('director_id');
+                    res.body.should.have.property('category');
+                    res.body.should.have.property('country');
+                    res.body.should.have.property('year');
+                    res.body.should.have.property('imdb_score');
+                    done();
+                });
+        });
+    });
+
 });
