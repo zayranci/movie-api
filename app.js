fed8fe23e9ea4c0bd4870936ddc6645260c1e8f3
zayranci <zayranci.za@gmail.com>
2019-01-29 13:42:43 +0300

    /index register[POST]

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
+const index = require('./routes/index');
 const movie= require('./routes/movie');
 const director= require('./routes/director');
 
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', index);
 app.use('/api/movies',movie);
 app.use('/api/directors',director);
 
